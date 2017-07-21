490bf8b8412f701fe01d8fdbeaaa32b648e4ee84
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2017-07-20 22:10:33 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 9999;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Add headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// routes will go here
+app.get('/', function(req, res) {
+  console.log(req.body);
+  res.send({ some: JSON.stringify({response:'json'}) });
+});
+
+app.post('/users', function(req, res) {
+  console.log(req.body);
+  res.send(req.body);
+
+});
+
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
